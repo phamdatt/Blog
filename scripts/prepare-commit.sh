042da2eecc1f88e69587452a81695466d552fa03
dat.pt <dat.pt@vn-cubesystem.com>
2023-12-01 22:25:35 +0700

    update

--- scripts/prepare-commit.sh
+++ scripts/prepare-commit.sh
@@ -1,7 +1,17 @@
-#!bash/bin
+#!/bin/bash
 
-## clear caching
-yarn clear:cache
 
-## running test before commit
-yarn test --coverage --watchAll=true || exit 0
+set -e
+
+G_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+echo 'precommit hook running...'
+echo "on branch $G_BRANCH"
+
+# we only care to keep dev clean
+if [ $G_BRANCH == 'develop' ]; then
+  yarn test:coverage &&\
+  npm run jestCoverageRatchet
+else
+  exit 0
+fi
